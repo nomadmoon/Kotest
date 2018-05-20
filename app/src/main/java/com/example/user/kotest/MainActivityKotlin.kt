740963a2d82f982c36bcaaa85d4076512ff84f54
nomadmoon <shade-khv@yandex.ru>
2018-05-20 19:48:05 +1000

    2

--- app/src/main/java/com/example/user/kotest/MainActivityKotlin.kt
+++ app/src/main/java/com/example/user/kotest/MainActivityKotlin.kt
@@ -1,11 +1,17 @@
 package com.example.user.kotest
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main_kotlin.*
 import org.jetbrains.anko.db.insert
+import java.io.Console
+import java.io.File
 
 class MainActivityKotlin : AppCompatActivity() {
 
@@ -25,6 +31,15 @@ class MainActivityKotlin : AppCompatActivity() {
             }
 
         }
+
+    fun btnShowDebug(v: View)
+    {
+        val tmpImg = File(getExternalFilesDir(null).absolutePath, "tmpImg.jpg")
+        val intentfoto = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intentfoto.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tmpImg))
+        startActivityForResult(intentfoto, 300)
+        Log.d("TAG", "zzzzzzzzzzzzz")
+    }
 
 }
 
